a4677c67896aeed6124574fb0a85b659bbe20dfc
Soumadip Banerjee <39thdecember@gmail.com>
2023-10-08 15:01:30 +0530

    adding dummy data

--- deploying_node_app/index.js
+++ deploying_node_app/index.js
@@ -1,24 +1,29 @@
 require('dotenv').config();
 const express = require('express'); //CommonJs syntax
+const dummyData = require('./dummyData');
 const app = express();
-const port = 4000;
+//const port = 4000;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
 app.get('/twitter', (req, res) => {
-    res.send('Hello TWITTER!');
+	res.send('Hello TWITTER!');
 });
 
 app.get('/login', (req, res) => {
-res.send('<h1>Please login at some WEBSITE</h1>')
+	res.send('<h1>Please login at some WEBSITE</h1>');
 });
 
 app.get('/youtube', (req, res) => {
-    res.send('<h2>Hello there from YouTube</h2>');
+	res.send('<h2>Hello there from YouTube</h2>');
+});
+
+app.get('/dummydata', (req, res) => {
+	res.json(dummyData);
 });
 
 app.listen(process.env.PORT, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${process.env.PORT}`);
 });
